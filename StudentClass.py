1bb1b09b0e09e1532cc4f2009bdfd56624b37357
rangersupport <122926917+rangersupport@users.noreply.github.com>
2023-12-03 22:48:23 -0500

    Add files via upload

---
+++ StudentClass.py
@@ -0,0 +1,46 @@
+# Student class defined with a validation constructor and a str method.
+
+class Student:
+    """Student class"""
+
+    def __init__(self, lname, fname, major, gpa=0.0):
+
+
+        # creation, raise an exception.
+        if lname == None:
+            raise Exception("__init__() missing 1 positional argument: 'lname'.")
+
+        # otherwise, initialize the instance variable last_name with lname
+        else:
+            self.last_name = lname
+
+
+        # creation, raise an exception.
+        if fname == None:
+            raise Exception("__init__() missing 1 positional argument: 'fname'.")
+
+
+        else:
+            self.first_name = fname
+
+
+        # creation, raise an exception.
+        if major == None:
+            raise Exception("__init__() missing 1 positional argument: 'major'.")
+        else:
+            self.major = major
+
+
+        if isinstance(type(gpa), float):
+            raise Exception("The  positional argument 'gpa' is not a float value.")
+
+        # Condition to check if the gpa is not in the range of 0 - 4 inclusively.
+        # If it is not in range, the raise a ValueError.
+        elif gpa < 0.0 or gpa > 4.0:
+            raise ValueError("The positional argument 'gpa' is not valid.")
+        else:
+            self.gpa = gpa
+
+    # method must return a string  of the Student details.
+    def __str__(self):
+        return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
---
+++ StudentTest.py
@@ -0,0 +1,75 @@
+# import the unittest module
+
+import unittest
+
+# import the class Student from the StudentClass file.
+
+from StudentClass import Student
+
+# test the Student class.
+class StudentClassTestCases (unittest.TestCase):
+     def setUp(self):
+         self.studObject = Student("Kim", "Kardashian", "Masters of Business Management", 3.5)
+
+     def tearDown(self):
+          del self.studObject
+     def test_object_created_required_attributes(self):
+
+          stud = Student("Kim", "Kardashian", "Masters of Business Management")
+          self.assertEqual(stud.last_name, "Kim")
+          self.assertEqual(stud.first_name, "Kardashian")
+          self.assertEqual(stud.major, "Masters of Business Management")
+          self.assertEqual(stud.gpa, 0.0)
+
+     def test_object_created_all_attributes(self):
+
+          stud = Student("Kim", "Kardashian", "Masters of Business Management", 3.5)
+          self.assertEqual(stud.last_name, "Kim")
+          self.assertEqual(stud.first_name, "Kardashian")
+          self.assertEqual(stud.major, "Masters of Business Management")
+          self.assertEqual(stud.gpa, 3.5)
+     def test_student_str(self):
+          self.assertEqual(str(self.studObject),
+                     "Kim, Kardashian has major Masters of Business Management with gpa: 3.5")
+     def test_object_not_created_error_last_name(self):
+          with self.assertRaises(TypeError):
+               stud = Student(fname=self.studObject.first_name, major=self.studObject.major, gpa=self.studObject.gpa)
+     def test_object_not_created_error_first_name(self):
+          with self.assertRaises(TypeError):
+               stud = Student(lname=self.studObject.last_name, major=self.studObject.major, gpa=self.studObject.gpa)
+     def test_object_not_created_error_major(self):
+          with self.assertRaises(TypeError):
+               stud = Student(lname=self.studObject.last_name, fname=self.studObject.first_name, gpa=self.studObject.gpa)
+
+
+     def test_object_not_created_error_gpa(self):
+
+          with self.assertRaises(ValueError):
+               stud = Student(lname=self.studObject.last_name, fname=self.studObject.first_name, major=self.studObject.major, gpa=-4.5)
+
+          with self.assertRaises(ValueError):
+               stud = Student(lname=self.studObject.last_name, fname=self.studObject.first_name, major=self.studObject.major, gpa=4.5)
+
+          with self.assertRaises(TypeError):
+               stud = Student(lname=self.studObject.last_name, fname=self.studObject.first_name, major=self.studObject.major, gpa="4.5")
+     def test_isinstance_of_gpa(self):
+          # test cases to test for isinstance
+          self.assertIsInstance(self.studObject.gpa, float)
+     # test cases to test the range
+     def test_range_of_gpa(self):
+          self.assertGreaterEqual(self.studObject.gpa, 0.0)
+          self.assertLessEqual(self.studObject.gpa, 4.0)
+
+# main execution
+if __name__ == '__main__':
+     # Define two Student objects
+     studObj1 = Student("Jose", "Rodriguez", "Crossing the Wall", 4.0)
+     studObj2 = Student("Jobs", "Steve", "The Best Fruit", 3.5)
+
+
+     # gets invoked.
+     print(studObj1)
+     print(studObj2)
+
+     # test all the test cases
+     unittest.main()
